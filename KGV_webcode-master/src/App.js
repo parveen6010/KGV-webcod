b02b6cd7248d87e31ea91ef553e6fc36d75d83c2
parveen prajapati <77872164+parveen6010@users.noreply.github.com>
2024-01-16 18:49:50 +0530

    parveen

--- KGV_webcode-master/src/App.js
+++ KGV_webcode-master/src/App.js
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
-import { Navbar } from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Booking } from "./pages/Booking";
-import { ContactUs } from "./pages/ContactUs";
-import { Product } from "./pages/Product";
-import { Footer } from "./components/Footer";
-const App = () => {
-  
-  return(
-    <div className="relative overflow-x-hidden">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/product" element={<Product/>}/>
-      <Route path="/booking" element={<Booking/>}/>
-      <Route path="/ContactUs" element={<ContactUs/>}/>
-    </Routes>
-    <Footer/>
+  import { Navbar } from "./components/Navbar";
+  import { Route, Routes } from "react-router-dom";
+  import { Home } from "./pages/Home";
+  import { Booking } from "./pages/Booking";
+  import { ContactUs } from "./pages/ContactUs";
+  import PaymentSuccess from "./pages/paymentSuccess";
+  const App = () => {
     
-    </div>
-  )
-};
+    return(
+      <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/booking" element={<Booking/>}/>
+        <Route path="/ContactUs" element={<ContactUs/>}/>
+        <Route path="/paymentsuccess" element={< PaymentSuccess />}   />
+      </Routes>
+      
+      </div>
+    )
+  };
 
-export default App;
+  export default App;
--- backend/contollers/paymentController.js
+++ backend/contollers/paymentController.js
@@ -9,7 +9,7 @@ export const checkout = async (req, res) => {
         currency: "INR",
     };
     const order = await instance.orders.create(options);
-
+    
     res.status(200).json({
         success: true,
         order,
@@ -17,38 +17,38 @@ export const checkout = async (req, res) => {
 };
 
 export const paymentVerification = async (req, res) => {
-    
-        const { razorpay_order_id, razorpay_payment_id, razorpay_signature , firstname , lastname , email , orderno, address ,phone } = req.body;
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature  } = req.body;
+    //     const { razorpay_order_id, razorpay_payment_id, razorpay_signature , firstname , lastname , email , orderno, address ,phone } = req.body;
 
-    //     const body = razorpay_order_id + "|" + razorpay_payment_id;
-    //     const expectedSignature = crypto
-    //     .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
-    //     .update(body.toString())
-    //     .digest("hex");
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
+        const expectedSignature = crypto
+        .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
+        .update(body.toString())
+        .digest("hex");
 
 
-    //     console.log("sig received ", razorpay_signature);
-    //     console.log("sig generated ", expectedSignature);
-    //  const isAuthentic = razorpay_signature === expectedSignature;
-     const isAuthentic = razorpay_order_id === razorpay_payment_id;
+        console.log("sig received ", razorpay_signature);
+        console.log("sig generated ", expectedSignature);
+     const isAuthentic = razorpay_signature === expectedSignature;
+    //  const isAuthentic = razorpay_order_id === razorpay_payment_id;
 
 
 
         if (isAuthentic) {
             // Database comes here
             
-            await Payment.create({
-                razorpay_order_id,
-                razorpay_payment_id,
-                razorpay_signature,
-                firstname,
-                 lastname, 
-                 email, 
-                 orderno, 
-                 address,
-                 phone
+            // await Payment.create({
+            //     razorpay_order_id,
+            //     razorpay_payment_id,
+            //     razorpay_signature,
+            //     firstname,
+            //      lastname, 
+            //      email, 
+            //      orderno, 
+            //      address,
+            //      phone
 
-                }); 
+            //     }); 
 
 
                 function sendEmailNotification(formData) {
@@ -60,16 +60,18 @@ export const paymentVerification = async (req, res) => {
                       },
                     });
                   
+                    // <p>Name: ${formData.name}</p>
+                    // <p>Lastname: ${formData.lastname}</p>
+                    // <p>Email: ${formData.email}</p>
+                    // <p>Address: ${formData.address}</p>
+                    // <p>Address: ${formData.orderno}</p>
                     const mailOptions = {
                       from: "parveenprajapati9310@gmail.com",
-                      to: formData.email,
+                      to: "parveenprajapati6010@gmail.com",
                       subject: "Customer booking Detail",
                       html: `<p>New registration details11:</p>
-                             <p>Name: ${formData.name}</p>
-                             <p>Lastname: ${formData.lastname}</p>
-                             <p>Email: ${formData.email}</p>
-                             <p>Address: ${formData.address}</p>
-                             <p>Address: ${formData.orderno}</p>
+                            
+
                              <p>razorpay_order_id: ${formData.razorpay_order_id}</p>
                              <p>razorpay_payment_id: ${formData.razorpay_payment_id}</p>`,
                     };
@@ -80,11 +82,8 @@ export const paymentVerification = async (req, res) => {
                         to: "ishantsah58@gmail.com",
                         subject: "Customer booking Detail",
                         html: `<p>New registration details11:</p>
-                               <p>Name: ${formData.name}</p>
-                               <p>Lastname: ${formData.lastname}</p>
-                               <p>Email: ${formData.email}</p>
-                               <p>Address: ${formData.address}</p>
-                               <p>Address: ${formData.orderno}</p>
+                              
+                        
                                <p>razorpay_order_id: ${formData.razorpay_order_id}</p>
                                <p>razorpay_payment_id: ${formData.razorpay_payment_id}</p>`,
                       };
@@ -113,34 +112,34 @@ export const paymentVerification = async (req, res) => {
             );
             } else {
 
-                function sendEmailNotification(formData) {
-                    const transporter = nodemailer.createTransport({
-                      service: "gmail",
-                      auth: {
-                        user: "parveenprajapati9310@gmail.com", // Update with your Gmail address
-                        pass: "davajvjvmpyfjlri", // Update with your Gmail password
-                      },
-                    });
+                // function sendEmailNotification(formData) {
+                //     const transporter = nodemailer.createTransport({
+                //       service: "gmail",
+                //       auth: {
+                //         user: "parveenprajapati9310@gmail.com", // Update with your Gmail address
+                //         pass: "davajvjvmpyfjlri", // Update with your Gmail password
+                //       },
+                //     });
                   
-                    const mailOptions = {
-                      from: "parveenprajapati9310@gmail.com",
-                      to: formData.email,
-                      subject: "Customer booking Detail",
-                      html: `<p>New registration details:</p>
-                             <p>payment failed</p>
-                             <p>again try</p>`,
-                    };
+                //     const mailOptions = {
+                //       from: "parveenprajapati9310@gmail.com",
+                //       to: formData.email,
+                //       subject: "Customer booking Detail",
+                //       html: `<p>New registration details:</p>
+                //              <p>payment failed</p>
+                //              <p>again try</p>`,
+                //     };
                   
-                    transporter.sendMail(mailOptions, function (error, info) {
-                      if (error) {
-                        console.log("Email error: " +error);
-                      } else {
-                        console.log("Email sent: " + info.response);
-                      }
-                    });
+                //     transporter.sendMail(mailOptions, function (error, info) {
+                //       if (error) {
+                //         console.log("Email error: " +error);
+                //       } else {
+                //         console.log("Email sent: " + info.response);
+                //       }
+                //     });
                   
-                  }
-                  sendEmailNotification(req.body);
+                //   }
+                //   sendEmailNotification(req.body);
                   
                 
             res.status(400).json({
